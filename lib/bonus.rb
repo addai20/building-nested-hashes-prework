8148845cbd4c64df5af4997a4eb90853e64fd705
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-18 20:22:24 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,10 +21,22 @@ def bonus
   }
 
   #code your solution here:
-  :heroine[status] = "dead"
-  :hero[status] = "dead"
-  
+  epic_tragedy[:hero["status"]] = "dead"
+ 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+
+# require_relative 'spec_helper'
+# require_relative '../lib/bonus.rb'
+
+# describe "#bonus" do 
+#   # remove the 'x' from the 'xit' below to activate this test!
+# 	it "uses the []= method to change the hero and heroine's status from alive to dead" do 
+#     expect(bonus[:montague][:hero][:status]).to eq("dead")
+#     expect(bonus[:capulet][:heroine][:status]).to eq("dead")
+    
+#   end
+# end
